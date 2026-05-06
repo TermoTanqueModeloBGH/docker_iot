9eca5d2aa2137e5c73e40327b094a8daa98ae04e
Julian <julianalmiron50@gmail.com>
2026-05-06 15:38:40 -0300

    Intento de interrupcion del teclado y verificacion de conexión al broker

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -1,28 +1,33 @@
 import asyncio, ssl, certifi, logging, os
 import aiomqtt
-
-logging.basicConfig(format='%(asctime)s - cliente mqtt - %(levelname)s:%(message)s', 
+#se coloca taskname para identificar la corrutina que se esta ejecutando
+logging.basicConfig(format='%(asctime)s -[%(taskname)s] - %(levelname)s:%(message)s', 
 level=logging.INFO, 
 datefmt='%d/%m/%Y %H:%M:%S %z')
 
-async def topico1(client,topic):
-    async with client.messages().filter(topic=topic) as messages:
-        async for message in messages:
-            logging.info(f"CORRUTINA {topic}: {message.payload.decode('utf-8')}")
 
 async def main():
+    #direccion del broker desde el entorno
+    broker = os.getenv('SERVIDOR')
     #Todo esto es el certificado
     tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
     tls_context.verify_mode = ssl.CERT_REQUIRED
     tls_context.check_hostname = True
     tls_context.load_default_certs()
 
-    async with aiomqtt.Client(
-        os.environ['SERVIDOR'],
+    try:
+        async with aiomqtt.Client(
+        broker,
         port=8883,
         tls_context=tls_context,
-    ) as client:
-        await client.subscribe(os.environ['TOPICO'])
+        ) as client:
+            logging.info("Conectado al broker MQTT: {broker}")
+            #suscribirse a un tema
+    except aiomqtt.MqttError:
+        logging.error(f"Error al conectar al broker MQTT")
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try: 
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logging.info("Cliente MQTT detenido a mano")
