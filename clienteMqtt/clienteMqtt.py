47a87edbe53c69edeba1f941ff77fd677be20a86
Julian <julianalmiron50@gmail.com>
2026-05-06 14:58:52 -0300

    Intentos de suscripciones

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -1,9 +1,17 @@
 import asyncio, ssl, certifi, logging, os
 import aiomqtt
 
-logging.basicConfig(format='%(asctime)s - cliente mqtt - %(levelname)s:%(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S %z')
+logging.basicConfig(format='%(asctime)s - cliente mqtt - %(levelname)s:%(message)s', 
+level=logging.INFO, 
+datefmt='%d/%m/%Y %H:%M:%S %z')
+
+async def topico1(client,topic):
+    async with client.messages().filter(topic=topic) as messages:
+        async for message in messages:
+            logging.info(f"CORRUTINA {topic}: {message.payload.decode('utf-8')}")
 
 async def main():
+    #Todo esto es el certificado
     tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
     tls_context.verify_mode = ssl.CERT_REQUIRED
     tls_context.check_hostname = True
@@ -15,8 +23,6 @@ async def main():
         tls_context=tls_context,
     ) as client:
         await client.subscribe(os.environ['TOPICO'])
-        async for message in client.messages:
-            logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
 
 if __name__ == "__main__":
     asyncio.run(main())
